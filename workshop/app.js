844e3be7955b34d201dced5033a372e83f291581
Quliyev02 <153994775+Quliyev02@users.noreply.github.com>
2024-04-27 15:07:21 +0400

    Add files via upload

---
+++ workshop/app.js
@@ -0,0 +1,60 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const MODEL_NAME = "gemini-1.5-pro-latest";
+  const API_KEY = "YOUR_API_KEY";
+  
+  async function run() {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+    const generationConfig = {
+      temperature: 1,
+      topK: 0,
+      topP: 0.95,
+      maxOutputTokens: 8192,
+    };
+  
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+  
+    const parts = [
+      {text: "write a js logic to filter the text in the array\n\n"},
+    ];
+  
+    const result = await model.generateContent({
+      contents: [{ role: "user", parts }],
+      generationConfig,
+      safetySettings,
+    });
+  
+    const response = result.response;
+    console.log(response.text());
+  }
+  
+  run();
+
+
+  
